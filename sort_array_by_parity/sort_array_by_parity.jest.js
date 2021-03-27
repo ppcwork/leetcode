0758f7e34e308ee0dec177f4c7afc6711717e876
Ifrah Sajjad <67663463+isajjad1246@users.noreply.github.com>
2021-03-27 10:53:18 -0400

    solution and test cases for sort_array_by_parity

---
+++ sort_array_by_parity/sort_array_by_parity.jest.js
@@ -0,0 +1,10 @@
+import {sortArrayByParity} from "./sort_array_by_parity.js"
+test("test 1", ()=>{
+    expect(sortArrayByParity([3, 1, 2, 4])).toEqual([4,2,3,1])
+})
+test("test 2", ()=>{
+    expect(sortArrayByParity([4, 8, 1, 3])).toEqual([8,4,1,3])
+})
+test("test 3", ()=>{
+    expect(sortArrayByParity([11, 11, 21, 4])).toEqual([4,11,11,21])
+})
